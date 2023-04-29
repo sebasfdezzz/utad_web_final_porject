ae6e3aa19447e9fb779429a9308423588f84240a
Sebastian Fernandez <seb.flipflip@gmail.com>
2023-04-29 02:52:42 +0200

    webapp

---
+++ webapp/user.js
@@ -0,0 +1,7 @@
+document.getElementById("webpages").addEventListener("click", function(){
+    window.location.href = "webpages.html";
+});
+
+document.getElementById("actions").addEventListener("click", function(){
+    window.location.href = "account_actions.html";
+});
